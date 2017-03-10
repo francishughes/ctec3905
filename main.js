ea189d26945ea8938794565d1ef5960f4e090ebd
francishughes <p14145079@my365.dmu.ac.uk>
2017-03-10 05:03:17 +0000

    Image slider using CSS
    Failed to use Jquery. So used CSS instead. Will look to use Javascript
    for other features later in the website

--- main.js
+++ main.js
@@ -1,31 +1,9 @@
-'use strict';
-
-$(function() {
-
-    //settings for slider
-    var width = 720;
-    var animationSpeed = 1000;
-    var pause = 3000;
-    var currentSlide = 1;
-
-    //cache DOM elements
-    var $slider = $('#slider');
-    var $slideContainer = $('.slides', $slider);
-    var $slides = $('.slide', $slider);
-
-    var interval;
-
-    function startSlider() {
-        interval = setInterval(function() {
-            $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
-                if (++currentSlide === $slides.length) {
-                    currentSlide = 1;
-                    $slideContainer.css('margin-left', 0);
-                }
-            });
-        }, pause);
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
     }
-    
+}
 
-
-});
